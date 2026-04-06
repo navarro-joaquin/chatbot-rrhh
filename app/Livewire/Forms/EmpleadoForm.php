d33cc991042b1b3923aae60d8eab407761e3437d
navarro-joaquin <quinchonavarro@gmail.com>
2026-04-06 14:40:59 -0400

    Correcciones con el formato de la fecha de contratacion

--- app/Livewire/Forms/EmpleadoForm.php
+++ app/Livewire/Forms/EmpleadoForm.php
@@ -38,8 +38,8 @@ class EmpleadoForm extends Form
     {
         return [
             'nombre_completo' => ['required', 'string', 'min:3', 'max:255'],
-            'carnet_identidad' => ['required', 'string', 'max:20', 'unique:empleados,carnet_identidad,' . ($this->empleado?->id ?? 'NULL')],
-            'telefono' => ['required', 'string', 'max:20', 'unique:empleados,telefono,' . ($this->empleado?->id ?? 'NULL')],
+            'carnet_identidad' => ['required', 'string', 'max:20', 'unique:empleados,carnet_identidad,'.($this->empleado?->id ?? 'NULL')],
+            'telefono' => ['required', 'string', 'max:20', 'unique:empleados,telefono,'.($this->empleado?->id ?? 'NULL')],
             'correo_electronico' => ['nullable', 'email', 'max:255'],
             'nro_item' => ['nullable', 'string', 'max:50'],
             'tipo' => ['required', 'in:Planta,Eventual'],
@@ -71,7 +71,7 @@ class EmpleadoForm extends Form
         $this->correo_electronico = $empleado->correo_electronico;
         $this->nro_item = $empleado->nro_item;
         $this->tipo = $empleado->tipo;
-        $this->fecha_contratacion = $empleado->fecha_contratacion;
+        $this->fecha_contratacion = $empleado->fecha_contratacion?->format('Y-m-d');
         $this->estado = (bool) $empleado->estado;
     }
 
